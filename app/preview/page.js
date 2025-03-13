66ea222d3d4771542a00a261212a04f5fdd6be26
Saurabh Wankhade <drsaurabh2018@gmail.com>
2025-03-13 19:55:01 +0530

    Progress bar added

--- app/preview/page.js
+++ app/preview/page.js
@@ -1,7 +1,6 @@
 'use client';
 import React from 'react'
 import Header from '../src/Components/Header'
-import Footer from '../src/Components/Footer'
 
 const page = () => {
   return (
@@ -9,7 +8,7 @@ const page = () => {
           <div className="lg:w-[640px] w-full flex flex-col border-[1px] relative h-full">
             {/*  Header */}
     
-            <Header />
+            <Header readOnly={true} />
     
             {/* Body */}
             <div className="overflow-y-auto scroll-smooth scrollbar-hide mt-10 sm:mb-12 mb-16 h-fit">
@@ -27,13 +26,17 @@ const page = () => {
     
                 {/* Submit button */}
 
-                
+            <div className='w-full mt-[24px] flex flex-col justify-end items-end'>
+              <button  className={` custom-shadow-add-que cursor-pointer flex items-center justify-end  gap-[4px] border-1 pt-[6px] pr-[16px] pb-[6px] pl-[14px] rounded-[12px]  bg-[#00AA45] border-[#1E874B] text-[#FFFFFF]`}>
+
+                <span className=' text-sm text-[##0D0D0D] font-[600] '>Save</span>
+              </button>
+
+            </div>
                 
               </div>
             </div>
     
-            {/* Footer */}
-            <Footer />
     
           </div>
         </div>
