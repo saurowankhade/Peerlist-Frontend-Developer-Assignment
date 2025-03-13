54f276d279d6f4c9d50008d66d02563a7801b942
Saurabh Wankhade <drsaurabh2018@gmail.com>
2025-03-14 00:44:17 +0530

    logs removed

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -4,9 +4,9 @@ import formSlices from "./formSlices";
 export const store = configureStore({
   reducer: {
     input: formSlices,
-    // Add more slices here if needed
+    
   },
-  devTools: process.env.NODE_ENV !== "production",
+  
 });
 
 export default store;
